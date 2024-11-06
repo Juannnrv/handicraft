a3b5ed5c23577b516c44d99a17b189ddfc24ce58
Juannnrv <160557063+Juannnrv@users.noreply.github.com>
2024-11-06 13:01:36 -0500

    chat
    (cherry picked from commit d533e63e1d7824da0f09dc7da69f88251551cc93)

--- server.js
+++ server.js
@@ -1,14 +1,32 @@
 const express = require('express');
-const app = express();
-const db = require('./server/helper/db');
+const http = require('http');
+const socketIo = require('socket.io');
 const cors = require('cors');
 
+const app = express();
 app.use(cors());
 app.use(express.json());
 
-db.getInstace();
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: "http://localhost:3000",
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log('Nuevo cliente conectado');
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Cliente desconectado');
+  });
+});
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => {
-    console.log(`Server running on port http://localhost:${port}`);
+server.listen(port, () => {
+  console.log(`Server running on port http://localhost:${port}`);
 });
