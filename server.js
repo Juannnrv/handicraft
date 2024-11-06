d6645855065ebb94c757a1ff5c9b2ec858718029
ealexandergarcia <106351807+ealexandergarcia@users.noreply.github.com>
2024-11-06 06:49:29 -0500

    prueba de que funciona

--- server.js
+++ server.js
@@ -11,4 +11,5 @@ db.getInstace();
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server running on port http://localhost:${port}`);
+    console.log("preuba");
 });
