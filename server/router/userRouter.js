fb2f7ff7ec2332d47976b64bdbf69f3ec9828d44
Juannnrv <160557063+Juannnrv@users.noreply.github.com>
2024-11-07 09:07:17 -0500

    feat: :construction: Working on create favorite

--- server/router/userRouter.js
+++ server/router/userRouter.js
@@ -1,13 +1,31 @@
 const express = require('express');
-const { createAccount, logIn, updateUser } = require('../controller/userController');
+const {
+  createAccount,
+  logIn,
+  updateUser,
+  findUserFavorites,
+  createUserFavorite,
+  findUserById,
+  findUserCoupons,
+  findUserOrders,
+  findUserWorkshopEnrollments,
+  deleteUserFavorite
+} = require('../controller/userController');
 const userValidator = require('../validator/userValidator');
 const { versioning } = require('../middleware/versioning');
 const { limit } = require('../middleware/limit');
 const verifyJwt = require('../middleware/authJwt');
-const user = express.Router();  
+const user = express.Router();
 
-user.post('/create', limit('post', '/create'), versioning('1.0.0'), userValidator.createAccount, createAccount);
-user.post('/login', limit('login', '/login'), versioning('1.0.0'), userValidator.logIn, logIn);
-user.put('/update/:id', verifyJwt, limit('put', '/update'), versioning('1.0.0'), userValidator.update, updateUser);
+user.post('/create', limit('post'), versioning('1.0.0'), userValidator.createAccount, createAccount);
+user.post('/login', versioning('1.0.0'), userValidator.logIn, logIn);
+user.post('/favorites', verifyJwt, limit('post'), versioning('1.0.0'), createUserFavorite);
+user.get('/', verifyJwt, limit('get'), versioning('1.0.0'), findUserById);
+user.get('/favorites', verifyJwt, limit('get'), versioning('1.0.0'), findUserFavorites);
+user.get('/coupons', verifyJwt, limit('get'), versioning('1.0.0'), findUserCoupons);
+user.get('/orders', verifyJwt, limit('get'), versioning('1.0.0'), findUserOrders);
+user.get('/workshop', verifyJwt, limit('get'), versioning('1.0.0'), findUserWorkshopEnrollments);
+user.put('/', verifyJwt, limit('put'), versioning('1.0.0'), userValidator.update, updateUser);
+user.delete('/favorites/:id', verifyJwt, limit('delete'), versioning('1.0.0'), deleteUserFavorite);
 
 module.exports = user;
