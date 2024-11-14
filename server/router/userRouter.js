85ec79ff47c4282ea844e2f851b55750a0b0192b
Gonzx77 <160796511+Gonzx77@users.noreply.github.com>
2024-11-14 07:44:56 -0500

    fix: :bug: No limit now

--- server/router/userRouter.js
+++ server/router/userRouter.js
@@ -9,13 +9,13 @@ const multer = require('multer');
 const upload = multer({ dest: 'uploads/' });
 const user = express.Router();
 
-user.post('/favorites', limit('post'), versioning('1.0.0'), createUserFavorite);
+user.post('/favorites', versioning('1.0.0'), createUserFavorite);
 user.get('/', limit('get'), versioning('1.0.0'), findUserById);
-user.get('/favorites', limit('get'), versioning('1.0.0'), findUserFavorites);
-user.get('/coupons', limit('get'), versioning('1.0.0'), findUserCoupons);
-user.get('/orders', limit('get'), versioning('1.0.0'), findUserOrders);
-user.get('/workshops', limit('get'), versioning('1.0.0'), findUserWorkshopEnrollments);
+user.get('/favorites', versioning('1.0.0'), findUserFavorites);
+user.get('/coupons', versioning('1.0.0'), findUserCoupons);
+user.get('/orders', versioning('1.0.0'), findUserOrders);
+user.get('/workshops', versioning('1.0.0'), findUserWorkshopEnrollments);
 user.put('/', upload.single('profilePicture'), limit('put'), versioning('1.0.0'), userValidator.update, updateUser);
-user.delete('/favorites/:id', limit('delete'), versioning('1.0.0'), deleteUserFavorite);
+user.delete('/favorites/:id', versioning('1.0.0'), deleteUserFavorite);
 
 module.exports = user;
