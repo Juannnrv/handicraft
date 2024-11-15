e859ea8a2c8791296513444509575bbf0a46cf1c
Gonzx77 <160796511+Gonzx77@users.noreply.github.com>
2024-11-14 20:15:51 -0500

    feat: :construction: Car view in progress

--- client/src/App.vue
+++ client/src/App.vue
@@ -7,6 +7,23 @@
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  mounted() {
+    if (!localStorage.getItem('car')) {
+      const car = {
+        userId: null,
+        products: [{productId: 12, quantity: 0, price: 0}, {productId: 13, quantity: 0, price: 0}],
+        total: 0,
+        date: null,
+        status: 'Pendiente',
+        coupons: [null]
+      };
+      
+      localStorage.setItem('car', JSON.stringify(car));
+      console.log('Objeto guardado en localStorage');
+    } else {
+      console.log('El objeto ya existe en localStorage');
+    }
+  }
 }
 </script>
